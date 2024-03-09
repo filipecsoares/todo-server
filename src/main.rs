9eb20f508f0bfe5c3879dd6498b4e1d228bdee8b
filipecsoares <filipecarlossoares@gmail.com>
2024-03-09 09:03:15 -0300

    test: test not found routes

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
-use actix_web::{HttpServer, App};
-use actix_web::{get, Responder, HttpResponse, web};
+use actix_web::{dev::Service, http::StatusCode, test, web, App, HttpResponse, HttpServer, Responder, get};
 
 #[get("/ping")]
 pub async fn ping() -> impl Responder {
    HttpResponse::Ok().body("pong")
 }
 
+#[get("/~/ready")]
+pub async fn readiness() -> impl Responder {
+    let process = std::process::Command::new("sh")
+        .arg("-c")
+        .arg("echo hello")
+        .output();
+    match process {
+        Ok(_) => HttpResponse::Accepted(),
+        Err(_) => HttpResponse::InternalServerError()
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
    HttpServer::new(||{
        App::new()
+           .service(readiness)
            .service(ping)
            .default_service(web::to(||HttpResponse::NotFound()))    
    })
@@ -18,4 +30,20 @@ async fn main() -> std::io::Result<()> {
    .unwrap()
    .run()
    .await
+}
+
+#[actix_web::test]
+async fn not_found_route() {
+    let app = test::init_service(
+        App::new()
+            .service(readiness)
+            .service(ping)
+            .default_service(web::to(|| HttpResponse::NotFound())),
+    )
+    .await;
+
+    let req = test::TestRequest::with_uri("/crazy-path").to_request();
+
+    let resp = app.call(req).await.unwrap();
+    assert_eq!(resp.status(), StatusCode::NOT_FOUND);
 }
